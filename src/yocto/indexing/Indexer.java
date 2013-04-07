fa9a76c2cfb4d567f0e06f09716d8fc31da6b4d5
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2013-04-07 23:28:08 +0300

    Introducing a first in-memory version of sorted sorted/grouped inverted index as a dictionary of term/postings-list entries.

--- src/yocto/indexing/Indexer.java
+++ src/yocto/indexing/Indexer.java
@@ -3,6 +3,9 @@ package yocto.indexing;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 import yocto.indexing.parsing.wikipedia.SAXWikipediaXMLDumpParser;
@@ -39,24 +42,76 @@ public class Indexer {
 
         System.out.println("\nIndexing Wikipedia documents\n");
         startTime = System.nanoTime();
-        String text;
-        HashSet<String> bagOfWords;
+        WikiPage doc;
+        HashSet<String> docTerms;
         Iterator<WikiPage> iter = docs.iterator();
+
+        // TODO The following is the data structure for that will hold
+        // an index by sorting and grouping. If I do the analysis concurrently
+        // then I have to make sure the data structures I use are synchronized.
+        // Better some concurrent collection.
+        // ATTENTION Sorting is guaranteed by using the TreeMap and grouping
+        //           by using a TreeSet of postings (we need postings sorted as
+        //           well that is why we are using a tree again HAVE TO IMPLEMENT
+        //           A COMPARATOR FOR THIS!!!!!
+        TreeMap<String, TreeSet<Posting>> invertedIndexSortedGrouped =
+                new TreeMap<String, TreeSet<Posting>>();
         while (iter.hasNext()) {
 
-            text = new String(iter.next().getRevisionText());
+            doc = iter.next();
             iter.remove();
 
-            bagOfWords = WikiPageAnalyzer.tokenizePageRevisionText(
-                    WikiPageAnalyzer.normalizePlainPageRevisionText(text),
+            docTerms = WikiPageAnalyzer.tokenizePageRevisionText(
+                    WikiPageAnalyzer.normalizePlainPageRevisionText(doc.getRevisionText()),
                     WikiPageAnalyzer.getInstanceStopwords());
+
+            TreeSet<Posting> termPostings;
+
+            // Iterate through all terms of the current document...
+            for (String term : docTerms) {
+                //... try to get the postings for the term in the inverted index...
+                termPostings = invertedIndexSortedGrouped.get(term);
+                if (termPostings == null) {
+                    // ...no term appears in the inverted index
+
+                    // prepare a new sorted list of postings
+                    TreeSet<Posting> addedPostings = new TreeSet<Posting>();
+                    // ... prepare a posting for the current document and add
+                    // it to the newly created sorted list of postings
+                    addedPostings.add(new Posting(doc.getId()));
+                    // and add a term/sorted-list-o-postings pair to the
+                    // inverted index.
+                    invertedIndexSortedGrouped.put(term, addedPostings);
+                }
+                else {
+                    // the term already appears in the inverted index so simply
+                    // add a new posting in the term's sorted list of postings.
+                    termPostings.add(new Posting(doc.getId()));
+                }
+
+
+            }
 
 
         }
+
         elapsedTime = System.nanoTime() - startTime;
         System.out.println("Total parsing time: " +
                 TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS) +
                 " seconds");
+
+        Set<String> terms = invertedIndexSortedGrouped.keySet();
+
+        for (String term : terms) {
+            System.out.print(term);
+            System.out.print(" ->");
+            TreeSet<Posting> postings = invertedIndexSortedGrouped.get(term);
+            for (Posting posting : postings) {
+                System.out.print(" " + posting.getDocId());
+            }
+            System.out.println();
+
+        }
     }
 
 }
---
+++ src/yocto/indexing/Posting.java
@@ -0,0 +1,103 @@
+package yocto.indexing;
+
+/**
+ * A class encapsulating the basic term-related information.
+ *
+ * Implements {@code Comparable<T>} in order to enable sets of the class to be
+ * preserved into sorted order by document id.
+ *
+ * Currently immutable.
+ *
+ * @author billy
+ */
+public class Posting implements Comparable<Posting> {
+
+    /** The document id number associated with this posting. */
+    private final long docId;
+
+    /** The term frequency of the term in th specific document. */
+    private final long tf;
+
+
+    /**
+     * Constructor.
+     *
+     * @param docId
+     *     The document id number with which this posting will be associated.
+     */
+    public Posting(long docId) {
+        this.docId = docId;
+        this.tf = 0;
+    }
+
+
+    // -- Getters
+
+
+    /**
+     * Gets the document id with which the posting is associated.
+     *
+     * @return
+     *     The document id.
+     */
+    public long getDocId() {
+        return docId;
+    }
+
+
+    // -- Overriding
+
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result
+                + (int) (docId ^ (docId >>> 32));
+
+        result = prime * result
+                + (int) (tf ^ (tf >>> 32));
+        return result;
+    }
+
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        Posting other = (Posting) obj;
+
+        return this.docId == other.docId;
+    }
+
+
+    /* (non-Javadoc)
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    @Override
+    public int compareTo(Posting other) {
+        final int LESS = -1;
+        final int EQUAL = 0;
+        final int GREATER = 1;
+
+        if (other == null) return GREATER;
+
+        // Enabling sorting by ascending document id.
+        if (this.docId > other.docId) return GREATER;
+        else if (this.docId < other.docId) return LESS;
+        else return EQUAL;
+    }
+
+}
